74682989620c029f780d40362eb550f5b87e2d17
Joakim <Deltaphish@users.noreply.github.com>
2015-11-06 23:57:35 +0100

    Made ai script
    Follows Gameobject with the tag "hero"

---
+++ Scripts/ai.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+public class ai : MonoBehaviour {
+	bool inrange = false;
+	float speed = 1.0f;
+	private Rigidbody br;
+	// Update is called once per frame
+	void Start () 
+	{
+		br = GetComponent<Rigidbody>();
+	}
+	void OnTriggerEnter(Collider player) 
+	{
+		inrange = true;	
+	
+	}
+	void OnTriggerExit(Collider player)
+	{
+		inrange = false;
+	}
+	void FixedUpdate() {
+		if (inrange == true)
+		{
+			Debug.Log("In Range");
+			Vector3 target = GameObject.FindGameObjectWithTag("hero").transform.position;
+			Vector3 current = GetComponent<Rigidbody>().position;
+			Vector3 movement = target - current;
+			br.AddForce(movement * speed);
+		}
+		else
+		{
+			Debug.Log("nope");
+			br.velocity = new Vector3(0.0f,0.0f,0.0f);
+		}
+}
+}
