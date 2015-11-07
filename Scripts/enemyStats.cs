2d553665c27969d86236175db4f60856149ee002
Lucas Kristiansson <luckri@hotmail.se>
2015-11-07 19:14:03 +0100

    stuff

---
+++ Scripts/enemyStats.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class enemyStats : MonoBehaviour {
+
+	public string name;
+	public int health;
+	public int attackSpeed;
+	public int level;
+	public int damage;
+	public int armor;
+	public EnemyType enemyType;
+	private int caveLevel;
+
+	public enum EnemyType {
+		MeleePhysical,
+		MeleeMagical,
+		RangedPhysical,
+		RangedMagical
+	}
+		
+	void Start () {
+
+	}
+
+	void Update () {
+		if (health <= 0) {
+			Dead();
+		} 
+	}
+
+	void Dead () {
+		Destroy(gameObject);
+	}
+
+	void Level () {
+		level = Random.Range (caveLevel - 2, caveLevel + 1);
+		if (level < 1) {
+			level = 1;
+		}
+	}
+}
