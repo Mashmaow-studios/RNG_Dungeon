225d377cd7c812392c81382512842e6e71bb55a5
Deltaphish <joakimanderlind@gmail.com>
2015-11-07 07:55:42 +0100

    added comments to ai2.cs

--- Scripts/ai2.cs
+++ Scripts/ai2.cs
@@ -5,11 +5,11 @@ public class ai2 : MonoBehaviour {
 	NavMeshAgent agent;
 	// Use this for initialization
 	void Start () {
+		//inits the agent
 		agent = GetComponent<NavMeshAgent> ();
 	}
-	
-	// Update is called once per frame
 	void Update () {
+		// sets the player as the target.
 		agent.SetDestination(GameObject.FindGameObjectWithTag("hero").transform.position);
 	}
 }
