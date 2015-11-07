b2be56c0b4f8165f69a8d8151a5cc6c33631e6bd
Lucas Kristiansson <luckri@hotmail.se>
2015-11-07 01:20:00 +0100

    Compressed code

--- Scripts/ai.cs
+++ Scripts/ai.cs
@@ -4,33 +4,29 @@ public class ai : MonoBehaviour {
 	bool inrange = false;
 	float speed = 1.0f;
 	private Rigidbody br;
-	// Update is called once per frame
-	void Start () 
-	{
+
+	void Start () {
 		br = GetComponent<Rigidbody>();
 	}
-	void OnTriggerEnter(Collider player) 
-	{
+
+	void OnTriggerEnter(Collider player) {
 		inrange = true;	
-	
 	}
-	void OnTriggerExit(Collider player)
-	{
+
+	void OnTriggerExit(Collider player){
 		inrange = false;
 	}
+
 	void FixedUpdate() {
-		if (inrange == true)
-		{
+		if (inrange == true) {
 			Debug.Log("In Range");
 			Vector3 target = GameObject.FindGameObjectWithTag("hero").transform.position;
 			Vector3 current = GetComponent<Rigidbody>().position;
 			Vector3 movement = target - current;
 			br.AddForce(movement * speed);
-		}
-		else
-		{
+		} else {
 			Debug.Log("nope");
 			br.velocity = new Vector3(0.0f,0.0f,0.0f);
 		}
-}
+	}
 }
