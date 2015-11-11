bf645b66b9ca9f3d6006aa7f355db4a07ca90e1c
Lucas Kristiansson <luckri@hotmail.se>
2015-11-11 19:40:04 +0100

    playerStats stuff

--- Scripts/playerstats.cs
+++ Scripts/playerstats.cs
@@ -5,26 +5,43 @@ public class PlayerStats : MonoBehaviour {
 	
 	private int level = 1, xp, xpNeeded, pastXpNeeded = 1000;
 	private int health = 10000, mana = 1000;
-
+	
 	void Start () {
 		xpNeeded = pastXpNeeded + level * 500;
 	}
-
+	
 	void Update () {
 		if(health <= 0)
 			Dead();
-
-
+		
 		if(xp >= xpNeeded)
 			LevelUp();
+		
+		if(Input.GetButtonDown(“e”)) {
+			health -= 1000;
+		}
 	}
-
+	
 	void LevelUp () {
 		xp -= xpNeeded;
+		pastXpNeeded = xpNeeded;
 		xpNeeded = pastXpNeeded + level * 500;
 	}
-
+	
+	void ReceiveXp (int receivedXp)  {
+		xp += receivedXp;
+	}
+	
 	void Dead () {
-
+		Time.timeScale = 0;
+	}
+	
+	void OnGUI () {
+		if(GUI.Button(new Rect(Screen.width/2-Screen.width/6, Screen.height/2-Screen.height/6, Screen.width/3, Screen.height/3), "Restart")) {
+			Time.timeScale = 1;
+			Application.LoadLevel(Application.loadedLevel);
+		}
+		
+		GUI.Label(new Rect(1, 1, 100, 100), health.ToString());
 	}
 }
